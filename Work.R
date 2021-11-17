b3726a911a77ca4c7e942df08f4c8f10af2b1184
Hans Joseph Masch <hjm224@lehigh.edu>
2021-11-17 15:44:10 -0500

    Updated sript

---
+++ Work.R
@@ -0,0 +1,8 @@
+#Load .csv data
+CovidData <- read.csv("pennsylvania-history-covid.csv")
+data.frame(CovidData)
+View(CovidData)
+
+TempData <- read.csv("Temperature History Data.csv")
+data.frame(TempData)
+View(TempData)
