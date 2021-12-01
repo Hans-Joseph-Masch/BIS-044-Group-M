4a06f4736f978da7837b094f9dcf6acb3cbe3bf9
Hans Joseph Masch <hjm224@lehigh.edu>
2021-11-30 20:56:21 -0500

    Newest Correlation Code Commit

--- Work.R
+++ Work.R
@@ -7,16 +7,16 @@ TempData <- read.csv("Temperature History Data 2.0.csv")
 data.frame(TempData)
 View(TempData)
 
-#
+#Assign date format
 CovidData$Date <- as.Date(CovidData$Date)
 TempData$Date <- as.Date(TempData$Date)
 CovidData$positiveCasesViral <- as.numeric(CovidData$positiveIncrease)
 str(CovidData$Date)
 str(TempData$Date)
 
-#Find a way to make the correlation
+#Merge Data sets
 DataSet <- merge(CovidData, TempData)
 View(DataSet)
 
-#
+#Correlate Data
 cor(DataSet$Temperature, CovidData$positiveIncrease)
