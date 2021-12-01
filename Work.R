3a6c97ceb697d8e34ccf80c2c56fe883935c440f
Hans Joseph Masch <hjm224@lehigh.edu>
2021-11-30 20:23:20 -0500

    Fixed .csv file

--- Work.R
+++ Work.R
@@ -3,6 +3,15 @@ CovidData <- read.csv("pennsylvania-history-covid.csv")
 data.frame(CovidData)
 View(CovidData)
 
-TempData <- read.csv("Temperature History Data.csv")
+TempData <- read.csv("Temperature History Data 2.0.csv")
 data.frame(TempData)
 View(TempData)
+
+#
+CovidData$date <- as.Date(CovidData$date)
+TempData$Date.time <- as.Date(TempData$Date.time)
+str(CovidData$date)
+str(TempData$Date.time)
+
+#Find a way to make the correlation 
+
